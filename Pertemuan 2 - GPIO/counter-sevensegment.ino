be886cc2c580f4f1db10925b24de2f2efe34775c
= <=>
2026-04-12 19:51:04 +0700

    fix: ubah pertemuan 2 jadi folder biasa

---
+++ Pertemuan 2 - GPIO/counter-sevensegment.ino
@@ -0,0 +1,58 @@
+#include <Arduino.h>
+
+const int segmentPins[8] = {7, 6, 5, 11, 10, 8, 9, 4};
+
+const int btnUp = 2;
+
+byte digitPattern[16][8] = {
+    {1, 1, 1, 1, 1, 1, 0, 0},
+    {0, 1, 1, 0, 0, 0, 0, 0},
+    {1, 1, 0, 1, 1, 0, 1, 0},
+    {1, 1, 1, 1, 0, 0, 1, 0},
+    {0, 1, 1, 0, 0, 1, 1, 0},
+    {1, 0, 1, 1, 0, 1, 1, 0},
+    {1, 0, 1, 1, 1, 1, 1, 0},
+    {1, 1, 1, 0, 0, 0, 0, 0},
+    {1, 1, 1, 1, 1, 1, 1, 0},
+    {1, 1, 1, 1, 0, 1, 1, 0},
+    {1, 1, 1, 0, 1, 1, 1, 0},
+    {0, 0, 1, 1, 1, 1, 1, 0},
+    {1, 0, 0, 1, 1, 1, 0, 0},
+    {0, 1, 1, 1, 1, 0, 1, 0},
+    {1, 0, 0, 1, 1, 1, 1, 0},
+    {1, 0, 0, 0, 1, 1, 1, 0}};
+
+int currentDigit = 0;
+
+bool lastUpState = HIGH;
+
+void displayDigit(int num)
+{
+    for (int i = 0; i < 8; i++)
+    {
+        digitalWrite(segmentPins[i], !digitPattern[num][i]);
+    }
+}
+
+void setup()
+{
+    for (int i = 0; i < 8; i++)
+    {
+        pinMode(segmentPins[i], OUTPUT);
+    }
+    pinMode(btnUp, INPUT_PULLUP);
+    displayDigit(currentDigit);
+}
+
+void loop()
+{
+    bool upState = digitalRead(btnUp);
+    if (lastUpState == HIGH && upState == LOW)
+    {
+        currentDigit++;
+        if (currentDigit > 15)
+            currentDigit = 0;
+        displayDigit(currentDigit);
+    }
+    lastUpState = upState;
+}
---
+++ Pertemuan 2 - GPIO/seven-segment.ino
@@ -0,0 +1,46 @@
+#include <Arduino.h>
+
+const int segmentPins[8] = {7, 6, 5, 11, 10, 8, 9, 4};
+
+byte digitPattern[16][8] = {
+    {1, 1, 1, 1, 1, 1, 0, 0},
+    {0, 1, 1, 0, 0, 0, 0, 0},
+    {1, 1, 0, 1, 1, 0, 1, 0},
+    {1, 1, 1, 1, 0, 0, 1, 0},
+    {0, 1, 1, 0, 0, 1, 1, 0},
+    {1, 0, 1, 1, 0, 1, 1, 0},
+    {1, 0, 1, 1, 1, 1, 1, 0},
+    {1, 1, 1, 0, 0, 0, 0, 0},
+    {1, 1, 1, 1, 1, 1, 1, 0},
+    {1, 1, 1, 1, 0, 1, 1, 0},
+    {1, 1, 1, 0, 1, 1, 1, 0},
+    {0, 0, 1, 1, 1, 1, 1, 0},
+    {1, 0, 0, 1, 1, 1, 0, 0},
+    {0, 1, 1, 1, 1, 0, 1, 0},
+    {1, 0, 0, 1, 1, 1, 1, 0},
+    {1, 0, 0, 0, 1, 1, 1, 0}};
+
+void displayDigit(int num)
+{
+  for (int i = 0; i < 8; i++)
+  {
+    digitalWrite(segmentPins[i], !digitPattern[num][i]);
+  }
+}
+
+void setup()
+{
+  for (int i = 0; i < 8; i++)
+  {
+    pinMode(segmentPins[i], OUTPUT);
+  }
+}
+
+void loop()
+{
+  for (int i = 0; i < 16; i++)
+  {
+    displayDigit(i);
+    delay(1000);
+  }
+}
